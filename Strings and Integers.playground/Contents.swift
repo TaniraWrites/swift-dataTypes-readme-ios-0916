4fa71b1066c0c3ef0ea0f77607db146acd3e0fc7
Taniraw <tanirawrites@gmail.com>
2016-08-30 11:01:56 -0400

    Strings and Integers Completed

---
+++ Strings and Integers.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+let thirdPlanet = "Earth"
+
+var ninthPlanet: String
+ninthPlanet = "Pluto"
+
+let fifthPlanet : String = "Jupiter"
+let sixthPlanet : String = "Saturn"
+let seventhPlanet = "Uranus"
+
+let numberOfMoonsOfSaturn = 62
+
+print("There are \(numberOfMoonsOfSaturn) orbiting \(sixthPlanet). ")
